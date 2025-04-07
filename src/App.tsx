9d60be086bb6284fbdae8523acafd511342070ae
AFAIVRE <afaivre@mac-afaivre.local>
2025-04-07 16:15:50 +0200

    Update: add new features

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
+import type { Schema } from "../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
 import { useAuthenticator } from '@aws-amplify/ui-react';
-import { useEffect, useState } from 'react';
-import { generateClient } from 'aws-amplify/data';
 
 const client = generateClient<Schema>();
 
@@ -18,7 +18,7 @@ function App() {
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
-
+  
   function deleteTodo(id: string) {
     client.models.Todo.delete({ id })
   }
@@ -28,20 +28,22 @@ function App() {
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map(todo => <li
+        {todos.map((todo) => (
+          <li 
           onClick={() => deleteTodo(todo.id)}
-          key={todo.id}>
-          {todo.content}
-        </li>)}
+          key={todo.id}>{todo.content}</li>
+        ))}
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/">Review next step of this tutorial.</a>
+        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+          Review next step of this tutorial.
+        </a>
       </div>
       <button onClick={signOut}>Sign out</button>
     </main>
-  )
-};
+  );
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -10,9 +10,9 @@ import '@aws-amplify/ui-react/styles.css';
 Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Authenticator>
-      <App />
-    </Authenticator>
-  </React.StrictMode>
+<React.StrictMode>
+  <Authenticator>
+    <App />
+  </Authenticator>
+</React.StrictMode>
 );
